b6c2405f4545771dd463507aef2706e8c701d8a6
tas284 <tiago.avila.saldanha@gmail.com>
2025-05-20 21:32:04 -0300

    add-posts-by-category

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -50,4 +50,75 @@ public class PostController : ControllerBase
             return StatusCode(500, new ResultViewModel<string>("Internal Server Error"));
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    [HttpGet("posts/{id:int}")]
+    public async Task<IActionResult> GetById(
+        [FromServices] BlogDataContext context,
+        int id)
+    {
+        try
+        {
+            var post = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .ThenInclude(x => x.Roles)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (post == null)
+                return NotFound(new ResultViewModel<string>("Post not found"));
+
+            return Ok(new ResultViewModel<Post>(post));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, new ResultViewModel<string>($"Internal Server Error: {e.Message}"));
+        }
+    }
+
+    [HttpGet("posts/category/{slug}")]
+    public async Task<IActionResult> GetByCategoryAsync(
+        [FromServices] BlogDataContext context,
+        [FromRoute] string slug,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var categories = await context.Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Where(x => x.Category.Slug == slug)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LasUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LasUpdateDate)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = categories.Count,
+                page,
+                pageSize,
+                categories
+            }));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, new ResultViewModel<string>($"Internal Server Error: {e.Message}"));
+        }
+    }
+>>>>>>> Stashed changes
 }
