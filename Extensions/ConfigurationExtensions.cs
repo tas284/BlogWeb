d8df794f657acb7d44db3710b03e1fd39b87bd78
tas284 <tiago.avila.saldanha@gmail.com>
2025-05-20 22:51:24 -0300

    add-connection-string

--- Extensions/ConfigurationExtensions.cs
+++ Extensions/ConfigurationExtensions.cs
@@ -4,6 +4,7 @@ using BlogWeb.Data;
 using BlogWeb.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -14,6 +15,8 @@ namespace BlogWeb.Extensions
     {
         public static WebApplicationBuilder AddConfiguration(this WebApplicationBuilder builder)
         {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<BlogDataContext>(x => x.UseSqlServer(connectionString));
             builder.Services.Configure<ApiConfiguration>(builder.Configuration.GetSection("ApiConfiguration"));
             builder.Services.Configure<SmtpConfiguration>(builder.Configuration.GetSection("SmtpConfiguration"));
             builder.Services.AddScoped<ApiKeyFilter>();
@@ -54,18 +57,12 @@ namespace BlogWeb.Extensions
 
         public static IServiceCollection ConfigureServices(this IServiceCollection services)
         {
-            services.AddDbContext<BlogDataContext>();
             services.AddTransient<TokenService>();
             services.AddTransient<EmailService>();
             services.AddMemoryCache();
-            services.AddResponseCompression(options =>
-            {
-                options.Providers.Add<GzipCompressionProvider>();
-            });
-            services.Configure<GzipCompressionProviderOptions>(options =>
-            {
-                options.Level = System.IO.Compression.CompressionLevel.Optimal;
-            });
+            services
+                .AddResponseCompression(options => options.Providers.Add<GzipCompressionProvider>())
+                .Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
             return services;
         }
     }
